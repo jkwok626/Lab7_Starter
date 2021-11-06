64ccef0ed46566cbbe8d6e0b14f4b3de55f5401e
Jackie Kwok <jackiekwok3@gmail.com>
2021-11-06 11:26:37 -0700

    Completed explore

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -57,6 +57,19 @@ function initializeServiceWorker() {
    *  TODO - Part 2 Step 1
    *  Initialize the service worker set up in sw.js
    */
+
+  // Checks if there is a service worker
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      // Register the service worker in sw.js when the page is loaded
+      navigator.serviceWorker.register('sw.js').then(function(registration) {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // If there is an error, the registration has failed
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
 }
 
 /**
--- sw.js
+++ sw.js
@@ -9,6 +9,34 @@ self.addEventListener('install', function (event) {
    * TODO - Part 2 Step 2
    * Create a function as outlined above
    */
+
+  // Store the files that we need to be cached in an array
+   var urlsToCache = [
+    '/',
+    'index.html',
+    'assets/styles/main.css',
+    'assets/scripts/main.js',
+    'assets/scripts/Router.js',
+    'assets/components/RecipeCard.js',
+    'assets/components/RecipeExpand.js',
+    'assets/images/icons/0-star.svg',
+    'assets/images/icons/1-star.svg',
+    'assets/images/icons/2-star.svg',
+    'assets/images/icons/3-star.svg',
+    'assets/images/icons/4-star.svg',
+    'assets/images/icons/5-star.svg',
+    'assets/images/icons/arrow-down.png',
+  ];
+
+  event.waitUntil(
+    // Open the cache
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        // Then pass in our array of files so that they can be added to the cache
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
 /**
@@ -21,6 +49,9 @@ self.addEventListener('activate', function (event) {
    * TODO - Part 2 Step 3
    * Create a function as outlined above, it should be one line
    */
+  
+  // Allows the service worker to control the pages immediately after registration
+  event.waitUntil(clients.claim());
 });
 
 // Intercept fetch requests and store them in the cache
@@ -29,4 +60,34 @@ self.addEventListener('fetch', function (event) {
    * TODO - Part 2 Step 4
    * Create a function as outlined above
    */
+  event.respondWith(
+    // When there is a request, we check if it matches with any file in our cache
+    caches.match(event.request)
+      .then(function(response) {
+        // If there's a match, then we return the response from the cache
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(
+          function(response) {
+            // Check if the response is valid
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clone the response because the browser will use up the response
+            // and the cache
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
 });
